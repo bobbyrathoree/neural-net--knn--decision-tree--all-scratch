9112d47cd967dd38e56305aa1e7001338172f2c7
Rathore, Bobby Singh Parmeshwardeen <brathore@iu.edu>
2019-12-15 23:02:53 -0500

    Update parse_text_files.py

--- parse_text_files.py
+++ parse_text_files.py
@@ -14,13 +14,8 @@ def parse_image_data(file_path: str):
             break
 
         each_line_split = image.split()
-        all_image_ids[str(idx)] = (
-            each_line_split[0],
-            int(each_line_split[1]),
-        )
-        data_vector.append(
-            [int(pixelValue) for pixelValue in each_line_split[2:]]
-        )
+        all_image_ids[str(idx)] = (each_line_split[0], int(each_line_split[1]))
+        data_vector.append([int(pixel_value) for pixel_value in each_line_split[2:]])
 
         idx += 1
         images_counter += 1
