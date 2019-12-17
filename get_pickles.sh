b43978763e3ad1d2d63b1230638bd4c6634aa113
Rathore, Bobby Singh Parmeshwardeen <brathore@iu.edu>
2019-12-17 01:58:32 -0500

    Update get_pickles.sh

--- get_pickles.sh
+++ get_pickles.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 wget -O pickles.zip "https://www.dropbox.com/s/fcpsycoa8f5s6ky/pickles.zip?dl=1" && unzip -q pickles.zip
+wget -O tree.pkl "https://www.dropbox.com/s/47bmiswe5x1pix6/tree.pkl?dl=1"
